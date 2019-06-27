4b9463a3d4238958b2673bf20cd126244fe5cfa6
qiurenbo <qiurenbo@outlook.com>
2019-06-27 21:50:00 +0800

    docs: change packge's name

--- README.md
+++ README.md
@@ -7,20 +7,20 @@ $ npm install dttrans
 
 ## Usage
 ```
-const dtransformer = require('dttrans');
+const dttrans = require('dttrans');
 
 
 const docs = [{'A': 1, 'B': 2, 'C':3}, {'A': 4, 'B': 3, 'C':5}];
-const table = dtransformer.docs2table(docs);
+const table = dttrans.docs2table(docs);
 //table: [['A', 'B', 'C'], [1, 2, 3], [4, 3, 5]];
 ```
 
 ```
-const dtransformer = require('dtransformer');
+const dttrans = require('dttrans');
 
 
 const table =  [['A', 'B', 'C'], [1, 2, 3], [4, 3, 5]];
-const docs = dtransformer.table2docs(table);
+const docs = dttrans.table2docs(table);
 //docs = [{'A': 1, 'B': 2, 'C':3}, {'A': 4, 'B': 3, 'C':5}];
 ```
 
--- test.js
+++ test.js
@@ -1,11 +1,11 @@
-const dtransformer = require('./index');
+const dttrans = require('./index');
 
 
 describe('docs2table',() => {
     describe('supported use', () => {
         test('structural data', () => {
             const docs = [{'A': 1, 'B': 2, 'C':3}, {'A': 4, 'B': 3, 'C':5}];
-            const table = dtransformer.docs2table(docs);
+            const table =dttrans.docs2table(docs);
             const expected = [['A', 'B', 'C'], [1, 2, 3], [4, 3, 5]];
             expect(table).toEqual(expected);
         });
@@ -14,12 +14,12 @@ describe('docs2table',() => {
     describe('unsupported use', () => {
         test('properties\'s names are not same', () => {
             const docs = [{'A': 1, 'B': 2, 'C':3}, {'D': 4, 'B': 3, 'C':5}];
-            expect(() => {dtransformer.docs2table(docs)}).toThrowError('docs aren\'t under same schema');
+            expect(() => {dttrans.docs2table(docs)}).toThrowError('docs aren\'t under same schema');
         });
     
         test('properties\'s orders are not same', () => {
             const docs = [{'A': 1, 'B': 2, 'C':3}, {'B': 1, 'A': 2, 'C':3}];
-            expect(() => {dtransformer.docs2table(docs)}).toThrowError('docs aren\'t under same schema');
+            expect(() => {dttrans.docs2table(docs)}).toThrowError('docs aren\'t under same schema');
         });
     })
 
@@ -30,7 +30,7 @@ describe('table2docs',() => {
     describe('supported use', () => {
         test('structural data', () => {
             const table =  [['A', 'B', 'C'], [1, 2, 3], [4, 3, 5]];
-            const docs = dtransformer.table2docs(table);
+            const docs =dttrans.table2docs(table);
             const expected = [{'A': 1, 'B': 2, 'C':3}, {'A': 4, 'B': 3, 'C':5}];
             expect(docs).toEqual(expected);
         });
@@ -39,7 +39,7 @@ describe('table2docs',() => {
     describe('unsupported use', () => {
         test('rows aren\'t under same schema', () => {
             const table =  [['A', 'B', 'C'], [1, 2], [4, 3, 5]];
-            expect(() => {dtransformer.table2docs(table)}).toThrowError('rows aren\'t under same schema');
+            expect(() => {dttrans.table2docs(table)}).toThrowError('rows aren\'t under same schema');
         });
     })
 
