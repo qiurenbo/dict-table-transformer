ecafe72039997fd1e67782509c1cbe487646b39d
qiurenbo <qiurenbo@outlook.com>
2019-06-28 21:13:26 +0800

    feat: add loose mode
    loose mode to handle structrual data with missing fields.

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-const docs2table = (docs) => {
+const docs2table = (docs, mode='stric') => {
     let table = [];
     let first = true;
     let header = [];
-    docs.forEach(doc => {
-        let row = [];
-        
-        // if it is a first doc, save its keys as table's header
+    docs.forEach((doc) => {
+
+        // If it is a first doc, save its keys as table's header.
         if(first){
             for (const key in doc) {
                 if (doc.hasOwnProperty(key)) {
@@ -16,21 +15,38 @@ const docs2table = (docs) => {
             table.push(header);
         }
 
-        let keys = [];
+        let row = [];
+        let keys = {};
         for (const key in doc) {
             if (doc.hasOwnProperty(key)) {
-                keys.push(key);
-                if(header.indexOf(key) === keys.indexOf(key)){
-                    row.push(doc[key]);
+                if(header.indexOf(key) > -1){
+                    row[header.indexOf(key)] = doc[key];
                 }
                 else{
                     throw new Error('docs aren\'t under same schema');
                 }
             }
+            keys[key] = 1;
         }
 
-        if(header.length !== keys.length){
-            throw new Error('docs aren\'t under same schema');
+
+        // Keys include key in both doc and header. iterate key in header to find
+        // the key in header and not in doc.
+        switch(mode){
+            case 'stric':
+                header.forEach(key => {
+                    if(!keys[key]){
+                        throw new Error('docs aren\'t under same schema');
+                    }
+                });
+                break;
+            case 'loose':
+                header.forEach(key => {
+                    if(!keys[key]){
+                        row[header.indexOf(key)] = '';
+                    }
+                });
+                break;
         }
 
         table.push(row);
@@ -41,12 +57,13 @@ const docs2table = (docs) => {
 
 
 
-const table2docs = (table) => {
+const table2docs = (table, mode='stric') => {
     let docs = [];
     let first = true;
     let properties = [];
 
     table.forEach(row => {
+        // First line is used to be properties.
         if(first){
             row.forEach(element => {
                 properties.push(element);
@@ -54,14 +71,34 @@ const table2docs = (table) => {
             first = !first;
         }
         else{
-            if (row.length !== properties.length){
-                throw new Error('rows aren\'t under same schema');
-            }
 
             let doc = {};
+
+            switch (mode) {
+                case 'stric':
+                    if (row.length !== properties.length){
+                        throw new Error('rows aren\'t under same schema');
+                    };             
+                    break;
+                case 'loose':
+                    if (row.length > properties.length){
+                        throw new Error('rows aren\'t under same schema');
+                    }
+                    else if (row.length < properties.length){
+                        properties.forEach(property => {
+                            doc[property] = '';
+                        });                   
+                    }
+                    
+                    // if row.length == properties.length do nothing
+                    break;  
+            }
+
+
             for (let i = 0; i < row.length; i++) {
                 doc[properties[i]] = row[i];
             }
+
 
             docs.push(doc);
         }
--- test.js
+++ test.js
@@ -2,52 +2,100 @@ const dttrans = require('./index');
 
 
 describe('docs2table',() => {
-    describe('supported use', () => {
-        test('structural data', () => {
+    describe('stric mode', () => {
+        test('structural data should be transformed right', () => {
             const docs = [{'A': 1, 'B': 2, 'C':3}, {'A': 4, 'B': 3, 'C':5}];
             const table =dttrans.docs2table(docs);
             const expected = [['A', 'B', 'C'], [1, 2, 3], [4, 3, 5]];
             expect(table).toEqual(expected);
         });
-    })
 
-    describe('unsupported use', () => {
-        test('properties\'s names are not same', () => {
-            const docs = [{'A': 1, 'B': 2, 'C':3}, {'D': 4, 'B': 3, 'C':5}];
+        test('the name of row property with same postion should be same', () => {
+            const docs = [{'A': 1, 'B': 2, 'C': 3}, {'D': 4, 'B': 3, 'C': 5}];
             expect(() => {dttrans.docs2table(docs)}).toThrowError('docs aren\'t under same schema');
         });
     
-        test('properties\'s orders are not same', () => {
-            const docs = [{'A': 1, 'B': 2, 'C':3}, {'B': 1, 'A': 2, 'C':3}];
+        test('the order of row property should not influence the result', () => {
+            const docs = [{'A': 1, 'B': 2, 'C':3}, {'B': 1, 'A': 2, 'C': 3}];
+            const expected = [['A', 'B', 'C'], [1, 2, 3], [2, 1, 3]];
+            expect(dttrans.docs2table(docs)).toEqual(expected);
+        });
+
+        test('the number of row properties should not be smaller to header\'s', () => {
+            const docs = [{'A': 1, 'B': 2, 'C': 3}, {'A': 4, 'B': 3}];
             expect(() => {dttrans.docs2table(docs)}).toThrowError('docs aren\'t under same schema');
         });
 
-        test('properties\'s number are not same', () => {
-            const docs = [{'A': 1, 'B': 2, 'C':3}, {'A': 4, 'B': 3}];
+        test('the number of row properties should not be larger to header\'s', () => {
+            const docs = [{'A': 1, 'B': 2, 'C': 3}, {'A': 1, 'B': 2, 'C': 3, 'D': 4}];
             expect(() => {dttrans.docs2table(docs)}).toThrowError('docs aren\'t under same schema');
         });
-    })
+
+    });
+
+    describe('loose mode', () => {
+        test('structural data should be transformed right', () => {
+            const docs = [{'A': 1, 'B': 2, 'C':3}, {'A': 4, 'B': 3, 'C':5}];
+            const table = dttrans.docs2table(docs, mode='loose');
+            const expected = [['A', 'B', 'C'], [1, 2, 3], [4, 3, 5]];
+            expect(table).toEqual(expected);
+        });
+
+        test('missing data should be filled right', () => {
+            const docs = [{'A': 1, 'B': 2, 'C':3}, {'C': 4, 'B': 3}];
+            const table = dttrans.docs2table(docs, mode='loose');
+            const expected = [['A', 'B', 'C'], [1, 2, 3], ['', 3, 4]];
+            expect(table).toEqual(expected);
+        });
+
+        test('the name of row property with same postion should be same', () => {
+            const docs = [{'A': 1, 'B': 2, 'C': 3}, {'D': 4, 'B': 3, 'C': 5}];
+            expect(() => {dttrans.docs2table(docs, mode='loose')}).toThrowError('docs aren\'t under same schema');
+        });
+    
+        test('the order of row property should not influence the result', () => {
+            const docs = [{'A': 1, 'B': 2, 'C':3}, {'B': 1, 'A': 2, 'C': 3}];
+            const expected = [['A', 'B', 'C'], [1, 2, 3], [2, 1, 3]];
+            expect(dttrans.docs2table(docs, mode='loose')).toEqual(expected);
+        });
+
+        test('the number of row properties should be smaller then header\'s', () => {
+            const docs = [{'A': 1, 'B': 2, 'C': 3}, {'A': 1, 'B': 2, 'C': 3, 'D': 5}];
+            expect(() => {dttrans.docs2table(docs, mode='loose')}).toThrowError('docs aren\'t under same schema');
+        });
+    });
 
 });
 
 
 describe('table2docs',() => {
-    describe('supported use', () => {
-        test('structural data', () => {
+    describe('stric mode',() => {
+        test('structural data should be transformed right', () => {
             const table =  [['A', 'B', 'C'], [1, 2, 3], [4, 3, 5]];
             const docs =dttrans.table2docs(table);
             const expected = [{'A': 1, 'B': 2, 'C':3}, {'A': 4, 'B': 3, 'C':5}];
             expect(docs).toEqual(expected);
         });
-    })
-
-    describe('unsupported use', () => {
-        test('rows aren\'t under same schema', () => {
+       
+        test('the number of row elements should be equal to header\'s', () => {
             const table =  [['A', 'B', 'C'], [1, 2], [4, 3, 5]];
             expect(() => {dttrans.table2docs(table)}).toThrowError('rows aren\'t under same schema');
         });
-    })
+    });
 
+    describe('loose mode',() => {
+        test('missing data should be filled right', () => {
+            const table =  [['A', 'B', 'C'], [1, 2], [4, 3, 5]];
+            const docs =dttrans.table2docs(table, mode='loose');
+            const expected = [{'A': 1, 'B': 2, 'C':''}, {'A': 4, 'B': 3, 'C':5}];
+            expect(docs).toEqual(expected);
+        });
+       
+        test('the number of row elements should be smaller than header\'s', () => {
+            const table =  [['A', 'B', 'C'], [1, 2, 4, 5], [4, 3, 5]];
+            expect(() => {dttrans.table2docs(table, mode='loose');}).toThrowError('rows aren\'t under same schema');
+        });
+    });
 });
 
 
